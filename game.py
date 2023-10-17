0121562a87d4c50cfead06cd0016b13a0f3ccd92
m260588 <m260588@usna.edu>
2023-10-16 22:11:25 -0400

    Blue Backgorund/sandy bottom

--- game.py
+++ game.py
@@ -1,2 +1,26 @@
-print('hello world')
-print('today is going to be a great day')
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+
+BLUE = (0, 0, 225)
+BROWN = (204, 129, 43)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Blue Background and sand bottom')
+
+running = True
+while running:
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           running = False
+   screen.fill(BLUE)
+   rectangle_height = 100
+   pygame.draw.rect(screen, BROWN, (0, screen_height-rectangle_height, screen_width, rectangle_height))
+
+   pygame.display.flip()
+
+pygame.quit()
